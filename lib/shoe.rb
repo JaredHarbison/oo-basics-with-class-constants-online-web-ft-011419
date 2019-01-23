6b0b401397dfd418f0103449229823a2715496f8
Jared Harbison <jared.harbison@gmail.com>
2019-01-23 18:02:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,11 +7,12 @@ class Shoe
   def initialize(brand)
     @brand = brand
     if BRANDS.include?(brand)
+    else 
+      BRANDS << brand 
+    end 
   end
   
  
-    BRANDS << brand 
-  end 
 
   def cobble
     self.condition = "new"
